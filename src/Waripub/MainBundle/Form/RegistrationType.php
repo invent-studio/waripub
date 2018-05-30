68366618b1523c6168c3e3fc5fa619cfc467ccf8
invent-studio <contact@invent-studio.com>
2018-05-30 20:27:12 +0000

    Mise à jour

---
+++ src/Waripub/MainBundle/Form/RegistrationType.php
@@ -0,0 +1,30 @@
+<?php
+// src/Waripub/MainBundle/Form/RegistrationType.php
+
+namespace Waripub\MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('tel');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
